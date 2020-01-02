6e983cc1bb2adf797d69fde577daf4f8700cc859
fahimcp495 <59098092+fahimcp495@users.noreply.github.com>
2020-01-03 01:47:17 +0600

    Add files via upload

---
+++ p001.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;	cin >> n;
+	long long sum = 0;
+	for (int i = 1; i < n; ++i){
+		if(i%3==0 || i%5==0)	sum += i;
+	}
+	cout << sum << endl;
+	return 0;
+}
+
+/*
+input: 10 ---> output: 23
+input: 1000 ---> output: 233168
